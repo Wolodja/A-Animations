bcfc1c62a0b3ab19732087e1d14ac5c5cedd5a7a
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-09-28 15:26:37 +0200

    Importing animations module and polyfills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { ZippyComponent } from './zippy/zippy.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TodosComponent } from './todos/todos.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs/observable/from';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
     ZippyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
