c579fc192c6fee2c40b89072be29593899ad50e1
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-10-01 09:47:38 +0200

    Separation animation

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,32 +1,11 @@
-import { trigger, state, transition, animate, style, group, query, useAnimation, animateChild, stagger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
+import { expandCollapse } from './zippy.component.animations';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
-  animations: [
-    trigger('expandCollapse', [
-      state('collapsed', style({
-        height: 0,
-        paddingTop: 0,
-        paddingBottom: 0,
-        opacity: 0
-      })),
-
-      transition('collapsed => expanded', [
-        animate('200ms ease-out', style({
-          height: '*',
-          paddingTop: '*',
-          paddingBottom: '*'
-        })),
-        animate('1s', style({ opacity: 1}))
-      ]),
-      transition('expanded => collapsed', [
-        animate('200ms ease-in')
-      ])
-    ])
-  ]
+  animations: [expandCollapse]
 })
 export class ZippyComponent {
   @Input('title') title: string;
