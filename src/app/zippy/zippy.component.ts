bf7fb13a586da972c2d8fb6e72bbcf4e9dd3ca3e
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-10-01 09:30:30 +0200

    Working with custom states

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,15 +1,33 @@
+import { trigger, state, transition, animate, style, group, query, useAnimation, animateChild, stagger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({
+        height: 0,
+        overflow: 'hidden',
+        paddingTop: 0,
+        paddingBottom: 0
+      })),
+
+      transition('collapsed => expanded', [
+        animate('200ms ease-out')
+      ]),
+      transition('expanded => collapsed', [
+        animate('200ms ease-in')
+      ])
+    ])
+  ]
 })
-export class ZippyComponent  {
+export class ZippyComponent {
   @Input('title') title: string;
   isExpanded: boolean;
 
-  toggle() { 
+  toggle() {
     this.isExpanded = !this.isExpanded;
   }
 }
